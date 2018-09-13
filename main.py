cb0a958c9f37aa6c73864bf41d578773952d9382
Joey <joey@xandem.com>
2018-09-12 21:42:20 -0600

    Added x axis. Noise gate re-applied.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_files = sorted(glob.glob("./csv/G*P0*.csv"))
+data_files = sorted(glob.glob("./csv/J*P0*.csv"))
 index_start = 5000
 index_length = 20000
 
-z_thresh = 20.0
+z_thresh = 10.0
 fS = 1000  # Sampling rate.
 fL = 50  # Cutoff frequency.
 N = 115  # Filter length, must be odd.
@@ -46,9 +46,9 @@ for file in data_files:
     force_z = force_z[index_start:index_start+index_length]
 
     #Noise gate
-    # force_x[np.abs(force_x) < z_thresh] = 0.0
-    # force_y[np.abs(force_y) < z_thresh] = 0.0
-    # force_z[np.abs(force_z) < z_thresh] = 0.0
+    force_x[np.abs(force_x) < z_thresh] = 0.0
+    force_y[np.abs(force_y) < z_thresh] = 0.0
+    force_z[np.abs(force_z) < z_thresh] = 0.0
     duty = np.count_nonzero(force_z) / float(len(force_z))
 
     #Calculations
@@ -59,9 +59,10 @@ for file in data_files:
 
     Ih = np.abs(force_y).sum()*dt
     Iv = np.abs(force_z).sum()*dt
-    T = Ih + Iv
+    Ix = np.abs(force_x).sum()*dt
+    T = Ih + Iv + Ix
     
-    print("{} - vel: {:.2f}, pace: {:.2f}, duty: {:.2f}, Ih: {:.2f}, Iv: {:.2f}, T: {:.2f}".format(file[6:], meters_per_second, min_per_mile, duty, Ih, Iv, T))
+    print("{} - vel: {:.2f}, pace: {:.2f}, duty: {:.2f}, Ih: {:.2f}, Iv: {:.2f}, Ix: {:.2f}, T: {:.2f}".format(file[6:], meters_per_second, min_per_mile, duty, Ih, Iv ,Ix, T))
     # plt.grid()
     # plt.plot(force_x, 'r', label='x')
     # plt.plot(force_y, 'b', label='y')
